afa7cc88869160328e67eaf84b12fb64d47df669
Shawn Analla <shawnanalla@gmail.com>
2022-02-09 12:02:26 -0800

    pulling

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,13 @@
+const express = require("express");
 const router = require('express').Router();
+const { User, Ponder } = require("../models");
 
+//Homepage -- TODO: should render some recent ponders on bottom left of interface in "recent ponders" section.
 router.get("/", (req, res) => {
     res.render("start");
   });
+
+//TODO: This
+router.get("/login", )
 
 module.exports = router;
