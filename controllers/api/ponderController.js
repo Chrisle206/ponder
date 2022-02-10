7ac2f5e652b724ae06f762a3e09db24a68825a6d
Christopher Le <Chrisle808@yahoo.com>
2022-02-10 10:14:24 -0800

    editting comment route

--- controllers/api/ponderController.js
+++ controllers/api/ponderController.js
@@ -3,21 +3,46 @@ const router = express.Router();
 const { User, Ponder } = require("../../models");
 
 router.get("/", (req, res) => {
-    Ponder.findAll().then(ponders => {
+    Ponder.findOne({
+        where: {id: Math.floor(Math.random()*Ponder.length) + 1}
+    }).then(ponders => {
+        res.json(ponders);
+    });
+});
+
+router.get("/", (req, res) => {
+    Ponder.findAll({
+         //map grab latest ponders
+    }).then(ponders => {
         res.json(ponders);
     });
 });
 
 router.post("/", (req, res) => {
-    Ponder.create({
-        body: req.body.body
-    }).then(ponders => {
-        res.json(ponders);
-    });
+    if (req.session.user) {
+        Tweet.create({
+          body: req.body.body,
+          UserId: req.session.user.id
+        }).then(newPost => {
+          res.json(newPost);
+        });
+    } else {
+        Tweet.create({
+            body: req.body.body,
+            UserId: "Anonymous"
+          }).then(newPost => {
+            res.json(newPost);
+          });
+    }
 });
+
 router.put();
 
-router.delete();
+router.delete("/", (req, res) => {
+    Ponder.destroy({
+        where: {id: req.params.id}
+    });
+});
 
 
 module.exports = router;
