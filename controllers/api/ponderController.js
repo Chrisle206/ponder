b26b27006684c84320c6b160190b83f9cfd565cf
Shawn Analla <shawnanalla@gmail.com>
2022-02-10 10:56:01 -0800

    pulling crhis

--- controllers/api/ponderController.js
+++ controllers/api/ponderController.js
@@ -2,13 +2,40 @@ const express = require("express");
 const router = express.Router();
 const { User, Ponder, Comment } = require("../../models");
 
-//TODO: POST route for creating a new ponder.
+//To use these routes, type localhost:3000/api/ponder as your base URL.
+//GET route for viewing a ponder.
+//TODO: Need a get route that will show ponders WITHOUT comments as well, for the 'recent ponders' aside.
+router.get("/:id", async (req, res) => {
+    try {
+        const postData = await Ponder.findByPk(req.params.id, {
+          include: [
+            User,
+            {
+              model: Comment,
+              include: [User],
+            },
+          ],
+        });
+    
+        if (postData) {
+          const post = postData.get({ plain: true });
+    
+          res.render('ponder', { post });
+        } else {
+          res.status(404).end();
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+
+//POST route for creating a new ponder. Currently doesn't allow for anonymous interaction but this will be fixed.
 router.post("/", async (req, res) => {
     if (req.session.user) {
         try {
             Ponder.create({
                 body: req.body.body,
-                userId: req.session.user.user
+                user_id: req.session.user.id
             }).then(newPost => {
                 res.json(newPost);
             });
--- seeds/seed.js
+++ seeds/seed.js
@@ -30,19 +30,30 @@ const ponders = [
 
 const comments = [
     {
-        body:"hello",
-        user_id:1
+        body:"This post sucks",
+        user_id:1,
+        ponder_id:1
     }
 ]
 
+const categories = [
+    {
+        category_name:"Random",
+    },
+    {
+        category_name:"Coding",
+    },
+]
+
 const sequelize = require("../config/connection");
-const {User, Ponder, Comment} = require("../models")
+const {User, Ponder, Comment, Category} = require("../models")
 
 const seed = async ()=>{
     await sequelize.sync({force:true});
     await User.bulkCreate(users,{individualHooks:true});
     await Ponder.bulkCreate(ponders);
     await Comment.bulkCreate(comments);
+    await Category.bulkCreate(categories);
     console.log("Database has been seeded!");
     process.exit(0);
 }
