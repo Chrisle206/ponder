fa2dda21e33b7f97d1b838b88986e484a23afe0e
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-02-13 20:25:30 -0800

    fix merge

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -1,22 +1,29 @@
-const upvoteBtn = document.querySelector('.upvote-btn');
-upvoteBtn.onclick = async (event) => {
-  event.preventDefault();
-  const ponderId = upvoteBtn.id;
-  // console.log("upvote button works");
-  const response = await fetch(`/api/ponder/upvote/${ponderId}`, {
-    method: 'PUT',
-    // headers: { 'Content-Type': 'application/json' },
-    // redirect: 'follow',
-  });
+const upvoteBtnArr = document.querySelectorAll('.upvote-btn');
 
-  if(window.location.pathname=="/" || window.location.pathname=="/active" ) {
-    //This is a catch so that upvoting/downvoting on the recent ponders aside does not redirect you to its specific page.
-    window.location.reload();
-  } else {
-    if (response.ok) {
-      window.location.replace(`/specific/${ponderId}`)
+// declares function
+const upvoteBtnClick = (upvoteBtn) => {
+  upvoteBtn.onclick = async (event) => {
+    event.preventDefault();
+    const ponderId = upvoteBtn.id;
+    // console.log("upvote button works");
+    const response = await fetch(`/api/ponder/upvote/${ponderId}`, {
+      method: 'PUT',
+      // headers: { 'Content-Type': 'application/json' },
+      // redirect: 'follow',
+    });
+
+    if(window.location.pathname=="/" || window.location.pathname=="/active" ) {
+      //This is a catch so that upvoting/downvoting on the recent ponders aside does not redirect you to its specific page.
+      window.location.reload();
     } else {
-      alert('Failed to upvote');
+      if (response.ok) {
+        window.location.replace(`/specific/${ponderId}`)
+      } else {
+        alert('Failed to upvote');
+      };
     };
-  };
+  }
 }
+
+// console.log(upvoteBtnArr);
+upvoteBtnArr.forEach(button => upvoteBtnClick(button));
